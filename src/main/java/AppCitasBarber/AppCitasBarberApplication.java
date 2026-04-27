e892517f62ecd736e4e3d422d6a47c4470152e57
Oscar Noel Nuño Verdín <oscarnoel1233@gmail.com>
2026-04-27 14:31:32 -0600

    Primer Commit

---
+++ src/main/java/AppCitasBarber/AppCitasBarberApplication.java
@@ -0,0 +1,13 @@
+package AppCitasBarber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppCitasBarberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppCitasBarberApplication.class, args);
+	}
+
+}
---
+++ src/test/java/AppCitasBarber/AppCitasBarberApplicationTests.java
@@ -0,0 +1,13 @@
+package AppCitasBarber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppCitasBarberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
